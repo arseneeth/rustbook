577bdb32311e49d518892a26b1d69b85ee8a21d7
0x332 <kinspotting@protonmail.com>
2019-09-02 16:11:27 +0300

    fix: delete binary

--- ch4/bindings/target/debug/bindings.d
+++
@@ -1,1 +0,0 @@
-/home/x332/Projects/rustbook/ch4/bindings/target/debug/bindings: /home/x332/Projects/rustbook/ch4/bindings/src/main.rs
--- ch4/bindings/target/debug/deps/bindings-7ab0237f087eb8f6.d
+++
@@ -1,5 +0,0 @@
-/home/x332/Projects/rustbook/ch4/bindings/target/debug/deps/bindings-7ab0237f087eb8f6: src/main.rs
-
-/home/x332/Projects/rustbook/ch4/bindings/target/debug/deps/bindings-7ab0237f087eb8f6.d: src/main.rs
-
-src/main.rs:
