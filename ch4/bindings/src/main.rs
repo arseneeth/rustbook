e4b9c009299d02c72130332d0f37c85799272c3c
0x332 <kinspotting@protonmail.com>
2019-09-02 16:10:32 +0300

    add: ch4

---
+++ ch4/bindings/src/main.rs
@@ -0,0 +1,12 @@
+
+
+fn main() {
+    let x = 5;
+
+    let (y, z) = (1, 2);
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+
+}
---
+++ ch4/bindings/target/debug/bindings.d
@@ -0,0 +1,1 @@
+/home/x332/Projects/rustbook/ch4/bindings/target/debug/bindings: /home/x332/Projects/rustbook/ch4/bindings/src/main.rs
