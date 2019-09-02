51015de859b01dd184ea6a9798267b5cc00d3c27
0x332 <kinspotting@protonmail.com>
2019-09-02 17:08:46 +0300

    add: types

---
+++ ch4/types/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+
+	// Booleans
+
+	let x = true;
+	let y: bool = false;
+
+	println!("x = {}",x);
+	println!("y = {}",y);
+
+	// Char
+
+	let x = 'x';
+	println!("x = {}",x);
+
+	// Numeric
+
+	let x = 42; // x has type i32
+
+	let y = 1.0; // y has type f64
+
+	println!("x = {}",x);
+	println!("y = {}",y);
+
+
+}
