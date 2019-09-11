7f6afd06b9c65b15d7f70cb45151364d6c82c335
0x332 <kinspotting@protonmail.com>
2019-09-11 19:21:07 +0800

    add: vectors

---
+++ ch4/vectors/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5]; // v: Vec<i32>
+
+    let vv = vec![0;10]; // ten zeroes
+
+
+    println!("The third element of vv is {}", vv[2]);
+    println!("The third element of v is {}", v[2]);
+}
