b16c2682258be5ffcd3e0c8f702e031cbf22170b
0x332 <kinspotting@protonmail.com>
2019-09-13 08:29:57 +0800

    add: more vector func

--- ch4/functions_2/src/main.rs
+++ ch4/functions_2/src/main.rs
@@ -67,4 +67,14 @@ fn main() {
 		println!("{}", x);
 	}
 
+	// Loop labels
+
+	'outer: for x in 0..10{
+		'inner: for y in 0..10{
+			if x % 2 == 0 { continue 'outer; } // continues the loop over x
+			if y % 2 == 0 { continue 'inner; } // continues the loop over y
+			println!("x: {}, y: {}", x, y);
+		}
+	}
+
 }
--- ch4/vectors/src/main.rs
+++ ch4/vectors/src/main.rs
@@ -1,9 +1,34 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5]; // v: Vec<i32>
+    let mut v = vec![1, 2, 3, 4, 5]; // v: Vec<i32>
 
     let vv = vec![0;10]; // ten zeroes
 
-
     println!("The third element of vv is {}", vv[2]);
     println!("The third element of v is {}", v[2]);
+
+    // It doesn't work
+	// let j: i32 = 0; 
+	// v[j];
+
+	// It does
+	let i: usize = 1;
+	println!("The second element of v is {}", v[i]);
+
+	match vv.get(7){
+		Some(x) => println!("Item 7 is {}", x),
+		None => println!("Sorry, this vector is too short.")
+	}
+
+	for i in &vv {
+		println!("A reference to {}", i);
+	}
+
+	for i in &mut v {
+		println!("A mutable reference to {}", i);
+	}
+
+	for i in v {
+		println!("Take ownership of the vector and its element {}", i);
+	}
+
 }
